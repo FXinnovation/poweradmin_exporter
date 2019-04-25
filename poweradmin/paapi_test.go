d90082c0051edd378fe22c15462ae3c1c10de1c7
Vincent Creusot <vincent.creusot@fxinnovation.com>
2019-04-25 08:30:49 -0400

    arranged tests

--- poweradmin/paapi_test.go
+++ poweradmin/paapi_test.go
@@ -22,7 +22,7 @@ func TestNewPAExternalAPIClient(t *testing.T) {
 func TestNewPAExternalAPIClient_GetMonitorInfos(t *testing.T) {
 	monitorString := `
 <monitors>
-<monitor id="8937" status="OK" depends_on="" title="Ping FXHTYUL1PMON001" lastRun="10-04-2019 13:18:28" nextRun="10-04-2019 13:19:28" errText="[Last response: 1 ms] " errActionIDs="570" fixedActionIDs="570" inErrSeconds="0" />
+<monitor id="8937" status="OK" depends_on="" title="Ping FXMACHINE1" lastRun="10-04-2019 13:18:28" nextRun="10-04-2019 13:19:28" errText="[Last response: 1 ms] " errActionIDs="570" fixedActionIDs="570" inErrSeconds="0" />
 </monitors>
 `
 
@@ -41,7 +41,7 @@ func TestNewPAExternalAPIClient_GetMonitorInfos(t *testing.T) {
 	assert.Equal(t, 1, len(monitors.Infos))
 	assert.Equal(t, "8937", monitors.Infos[0].ID)
 	assert.Equal(t, "OK", monitors.Infos[0].Status)
-	assert.Equal(t, "Ping FXHTYUL1PMON001", monitors.Infos[0].Title)
+	assert.Equal(t, "Ping FXMACHINE1", monitors.Infos[0].Title)
 	assert.Equal(t, 2019, monitors.Infos[0].LastRun.Year())
 }
 
@@ -60,7 +60,7 @@ func TestNewPAExternalAPIClient_GetMonitorInfos_BadUrl(t *testing.T) {
 func TestNewPAExternalAPIClient_GetMonitorInfos_UnmarshalError(t *testing.T) {
 	monitorString := `
 <monitors>
-<monitor id="8937" status="OK" depends_on="" title="Ping FXHTYUL1PMON001" lastRun="NoTime 13:18:28" nextRun="10-04-2019 13:19:28" errText="[Last response: 1 ms] " errActionIDs="570" fixedActionIDs="570" inErrSeconds="0" />
+<monitor id="8937" status="OK" depends_on="" title="Ping" lastRun="NoTime 13:18:28" nextRun="10-04-2019 13:19:28" errText="[Last response: 1 ms] " errActionIDs="570" fixedActionIDs="570" inErrSeconds="0" />
 </monitors>
 `
 
@@ -102,10 +102,7 @@ func TestNewPAExternalAPIClient_GetGroupList(t *testing.T) {
 <?xml version="1.0"?>
 <groups>
     <group name="Servers/Devices" path="Servers/Devices" id="0" parentID="-1"/>
-    <group name="Centris" path="Servers/Devices^Live^Centris" id="2" parentID="647"/>
-    <group name="FX Hosting" path="Servers/Devices^Live^FX Hosting" id="8" parentID="647"/>
-    <group name="FXHT 3000 RL IDS [YUL1]" path="Servers/Devices^Live^FX Hosting^Physical^FXHT 3000 RL IDS [YUL1]" id="9" parentID="192"/>
-    <group name="CTRI 1250 RL MTL [YUL1]" path="Servers/Devices^Live^Centris^Physical^CTRI 1250 RL MTL [YUL1]" id="16" parentID="92"/>
+    <group name="Central" path="Servers/Devices^Live^Central" id="2" parentID="647"/>
 </groups>
 `
 
@@ -121,10 +118,10 @@ func TestNewPAExternalAPIClient_GetGroupList(t *testing.T) {
 	client, _ := NewPAExternalAPIClient("1234key", ts.URL)
 	groups, err := client.GetGroupList()
 	assert.Nil(t, err)
-	assert.Equal(t, 5, len(groups.Groups))
+	assert.Equal(t, 2, len(groups.Groups))
 	assert.Equal(t, "2", groups.Groups[1].ID)
-	assert.Equal(t, "Centris", groups.Groups[1].Name)
-	assert.Equal(t, "Servers/Devices^Live^Centris", groups.Groups[1].Path)
+	assert.Equal(t, "Central", groups.Groups[1].Name)
+	assert.Equal(t, "Servers/Devices^Live^Central", groups.Groups[1].Path)
 	assert.Equal(t, "647", groups.Groups[1].ParentID)
 }
 
@@ -132,13 +129,9 @@ func TestNewPAExternalAPIClient_GetServerList(t *testing.T) {
 	serverListString := `
 <?xml version="1.0"?>
 <servers>
-<server name="FXHTYUL1PMON001" group="Servers/Devices^Live^FX Hosting^Physical^FXHT 3000 RL IDS [YUL1]^Prod" id="568" groupID="193" alias="FXHTYUL1PMON001" status="ok"/>
-<server name="FXHTYUL1PMON002" group="Servers/Devices^Live^FX Hosting^Physical^FXHT 3000 RL IDS [YUL1]^Prod" id="709" groupID="193" alias="FXHTYUL1PMON002" status="ok"/>
-<server name="FXHTYUL1PSQL001" group="Servers/Devices^Live^FX Hosting^Physical^FXHT 3000 RL IDS [YUL1]^Prod" id="710" groupID="193" alias="FXHTYUL1PSQL001" status="ok"/>
-<server name="FXHTYUL1PSQL002" group="Servers/Devices^Live^FX Hosting^Physical^FXHT 3000 RL IDS [YUL1]^Prod" id="711" groupID="193" alias="FXHTYUL1PSQL002" status="disabled"/>
-<server name="FXHTYUL1PESX001" group="Servers/Devices^Live^FX Hosting^Physical^FXHT 3000 RL IDS [YUL1]^Prod" id="838" groupID="193" alias="FXHTYUL1PESX001" status="ok"/>
-<server name="FXHTYUL1PESX002" group="Servers/Devices^Live^FX Hosting^Physical^FXHT 3000 RL IDS [YUL1]^Prod" id="870" groupID="193" alias="FXHTYUL1PESX002" status="ok"/>
-<server name="FXHTYUL1PESX003" group="Servers/Devices^Live^FX Hosting^Physical^FXHT 3000 RL IDS [YUL1]^Prod" id="871" groupID="193" alias="FXHTYUL1PESX003" status="ok"/>
+<server name="FXH1" group="Servers/Devices^Live^FX Hosting^Physical^FX^Prod" id="568" groupID="193" alias="FXH1" status="ok"/>
+<server name="FXH2" group="Servers/Devices^Live^FX Hosting^Physical^FX^Prod" id="709" groupID="193" alias="FXH2" status="ok"/>
+<server name="FXH3" group="Servers/Devices^Live^FX Hosting^Physical^FX^Prod" id="710" groupID="193" alias="FXH3" status="ok"/>
 </servers>
 `
 
@@ -154,13 +147,13 @@ func TestNewPAExternalAPIClient_GetServerList(t *testing.T) {
 	client, _ := NewPAExternalAPIClient("1234key", ts.URL)
 	servers, err := client.GetServerList("193")
 	assert.Nil(t, err)
-	assert.Equal(t, 7, len(servers.Servers))
+	assert.Equal(t, 3, len(servers.Servers))
 	assert.Equal(t, "568", servers.Servers[0].ID)
-	assert.Equal(t, "FXHTYUL1PMON001", servers.Servers[0].Name)
-	assert.Equal(t, "Servers/Devices^Live^FX Hosting^Physical^FXHT 3000 RL IDS [YUL1]^Prod", servers.Servers[0].Group)
+	assert.Equal(t, "FXH1", servers.Servers[0].Name)
+	assert.Equal(t, "Servers/Devices^Live^FX Hosting^Physical^FX^Prod", servers.Servers[0].Group)
 	assert.Equal(t, "193", servers.Servers[0].GroupID)
 	assert.Equal(t, "ok", servers.Servers[0].Status)
-	assert.Equal(t, "FXHTYUL1PMON001", servers.Servers[0].Alias)
+	assert.Equal(t, "FXH1", servers.Servers[0].Alias)
 }
 
 func TestNewPAExternalAPIClient_NoApiKey(t *testing.T) {
