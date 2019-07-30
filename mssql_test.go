060150e18f3ae1eb696c3f3b9034e09e754e92d2
Vincent Creusot <vincent.creusot@fxinnovation.com>
2019-07-30 13:25:20 -0700

    added testts

--- mssql_test.go
+++ mssql_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql/driver"
 	"github.com/DATA-DOG/go-sqlmock"
+	"reflect"
 	"testing"
 	"time"
 )
@@ -31,5 +32,109 @@ func TestSqlServerConnection_GetStatData(t *testing.T) {
 	// we make sure that all expectations were met
 	if err := mock.ExpectationsWereMet(); err != nil {
 		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestSQLServerConnection_GetConfigComputerInfo(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+	connection := &SQLServerConnection{
+		conn: db,
+	}
+	rows := sqlmock.NewRows([]string{"CompID", "Name", "Alias", "GroupID"}).
+		AddRow(2, "FXSERVER", "FXSERVER", 1)
+	mock.ExpectQuery("SELECT (.+) FROM ConfigComputerInfo WHERE Alias = (.+)$").WillReturnRows(rows)
+	ci, err := connection.GetConfigComputerInfo("FXSERVER")
+	if err != nil {
+		t.Fatalf("Get CI returned an error:%v", err)
+	}
+	if ci.CompID != 2 {
+		t.Fatalf("Get CI returned bad data")
+	}
+	// we make sure that all expectations were met
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestSQLServerConnection_GetAllServersFor(t *testing.T) {
+
+	type sqlReturn struct {
+		CompID  int
+		Name    string
+		Alias   string
+		GroupID int
+	}
+	type fields struct {
+		connection *SQLServerConnection
+		rows       []sqlReturn
+	}
+	type args struct {
+		group string
+	}
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+	connection := &SQLServerConnection{
+		conn: db,
+	}
+
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    []string
+		wantErr bool
+	}{
+		{
+			name: "gotresult",
+			fields: fields{
+				connection: connection, rows: []sqlReturn{{CompID: 2, Name: "FXSERVER", Alias: "FXSERVER", GroupID: 1}},
+			},
+			args: args{
+				group: "Servers/Devices^Live^Dev",
+			},
+			want: []string{
+				"FXSERVER",
+			},
+			wantErr: false,
+		},
+		{
+			name: "noresult",
+			fields: fields{
+				connection: connection, rows: []sqlReturn{},
+			},
+			args: args{
+				group: "NoReturn",
+			},
+			want:    nil,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rows := sqlmock.NewRows([]string{"CompID", "Name", "Alias", "GroupID"})
+			for _, r := range tt.fields.rows {
+				rows.AddRow(r.CompID, r.Name, r.Alias, r.GroupID)
+			}
+			const expectedSQL = "SELECT (.*) FROM ConfigComputerInfo CI INNER JOIN (.+) GI ON CI.GroupID = GI.GroupID WHERE GI.ParentGroupPath = (.+)"
+			mock.ExpectQuery(expectedSQL).WillReturnRows(rows)
+			got, err := tt.fields.connection.GetAllServersFor(tt.args.group)
+			// we make sure that all expectations were met
+			if err := mock.ExpectationsWereMet(); err != nil {
+				t.Errorf("there were unfulfilled expectations: %s", err)
+			}
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetAllServersFor() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetAllServersFor() got = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
